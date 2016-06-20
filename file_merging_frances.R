0c16c715925f8ad749a93298c1fca6f9910c291e
cflagg <cflagg@neoninc.org>
2016-06-20 13:30:01 -0600

    file merging for TSV

--- file_merging_frances.R
+++ file_merging_frances.R
@@ -6,7 +6,7 @@ library(stringr)
 library(plyr)
 
 # point to your folder with all the files
-directory <- "C:/Users/cflagg/Documents/GitHub/NEON-Internship-2016/fileMergeExample"
+directory <- "C:/Users/cflagg/Documents/GitHub/NEON-Internship-2016/taxonTables"
 
 ## OPTION 1: if all of your files are in one folder, just set the working directory
 ## R only automatically looks at files in a few folders all the time, including your 'working directory'
@@ -16,7 +16,7 @@ setwd(directory)
 files <- list.files(directory, full.names = FALSE)
 
 # check if it works -- open first file
-read.csv(files[1])
+head(read.table(files[1], header = TRUE, fill = TRUE, quote = "", sep="\t"))
 
 ## OPTION 2: if your files are in multiple folders, point to the 'root' folder and grab the full file path
 # files <- list.files(directory, full.names = TRUE)
@@ -25,18 +25,22 @@ read.csv(files[1])
 # the file list here should point to "fileGrab1" for this specific script
 multiCombine <- function(input, ply = ldply){
   ply(input, function(x){
-    t <- read.csv(x, header=TRUE, sep=",",stringsAsFactors = FALSE) # read the csv
+    t <- read.table(x, header=TRUE, sep="\t",fill = TRUE, quote = "") # read the tsv
     ## see what x is
     print(x)
     ## append part of the fileName as a column ## FIX THIS ##
-    t$newCol <- str_sub(x, 1,3)
+    t$siteID <- stringr::str_sub(x, 1,4)
+    ## add mgrast ID from file name
+    t$mgrastID <- stringr::str_sub(x,1,10)
+    colnames(t) <- c("species", "count", "siteID", "mgrastID")
     t1 <- rbind(t) # rbind it to a temporary variable
     return(t1) # return the full variable
   }
   )
 }
 
-# a re-usable function - outputType can be 'vector' or 'list'
+# OPTION 2: a re-usable function - outputType can be 'vector' or 'list' 
+## use if you need files in more than one folder
 fileParser <- function(directoryList, outputType = "vector"){
   # need to dive into each folder, decide if it's a .csv or .xlsx, then read the rows
   fullFilenameList <- list() # initialize a list object to populate
